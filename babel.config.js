40ecc8cd52f6e46ee25750974906c4b4fa23eb2c
Rajesh <rajeshsarkar736146@gmail.com>
2026-05-30 01:32:07 +0530

    fix: add reanimated babel plugin - required for production builds

--- babel.config.js
+++ babel.config.js
@@ -6,5 +6,8 @@ module.exports = (api) => {
       ['babel-preset-expo', { jsxImportSource: 'nativewind', unstable_transformProfile: 'hermes-stable' }],
       'nativewind/babel',
     ],
+    plugins: [
+      'react-native-reanimated/plugin',
+    ],
   };
 };
